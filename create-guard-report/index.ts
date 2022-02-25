02e5d5d10c2f1c1d18c0680ddac26fc5c5b72a08
Esa Firman <esafirm21@gmail.com>
2022-02-25 15:43:47 +0700

    update the script

--- create-guard-report/index.ts
+++ create-guard-report/index.ts
@@ -5,12 +5,15 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 //@ts-ignore
-const inputPath = 'usage.txt';
+const inputPath = process.argv[2] || 'usage.txt';
 if (!fs.existsSync(inputPath)) {
-  throw new Error('You must have usage.txt in the root of directory');
+  throw new Error(
+    'You must have usage.txt in the root of directory or provide it with create-guard-report <usage file>'
+  );
 }
 
-const branch = 'ts_parser';
+const branch = process.argv[3] || 'main';
+
 const zipFile = `https://github.com/esafirm/guard-report/archive/refs/heads/${branch}.zip`;
 const targetFile = '/tmp/template.zip';
 const targetDir = '/tmp/cgr-template/';
@@ -18,11 +21,14 @@ const realTargetDir = `${targetDir}guard-report-${branch}`;
 
 const options = { stdio: 'ignore' };
 
+// Printing info
+console.log(`Using ${inputPath} as the input`);
+
 // Prepare env
 execSync(`rm -rf ${targetFile} ${targetDir}`, options);
 
 // Download the zip
-console.log('Downloading the template…');
+console.log(`Downloading the template for branch ${branch}…`);
 execSync(`curl -L ${zipFile} --output ${targetFile}`, options);
 
 // Extract the zip and delete
@@ -39,7 +45,8 @@ execSync(`cp -a ${inputPath} ${realTargetDir}`);
 // Creating the report
 console.log('Creating the report…');
 execSync(
-  `cd ${realTargetDir} && npm run create-report && mv ${targetDir}/build/index.html .`
+  `cd ${realTargetDir} && npm run create-report && mv ${realTargetDir}/build/index.html .`,
+  options
 );
 
 // Cleanup
