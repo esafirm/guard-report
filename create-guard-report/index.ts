65f27db7e0667ae4434699af129c68a6cab6f63a
Esa Firman <esafirm21@gmail.com>
2022-02-26 09:52:49 +0700

    Ts parser (#1)
    * initial ts parser
    
    * make the script faster
    
    * handle writing file gracefully
    
    * working as expected
    
    * create a separate command for generating the report in one go
    
    * fix create report command
    
    * npm install silent
    
    * update the script
    
    * change output file name
    
    * output the output file path

---
+++ create-guard-report/index.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+//@ts-ignore
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+//@ts-ignore
+const inputPath = process.argv[2] || 'usage.txt';
+if (!fs.existsSync(inputPath)) {
+  throw new Error(
+    'You must have usage.txt in the root of directory or provide it with create-guard-report <usage file>'
+  );
+}
+
+const branch = process.argv[3] || 'main';
+
+const zipFile = `https://github.com/esafirm/guard-report/archive/refs/heads/${branch}.zip`;
+const targetFile = '/tmp/template.zip';
+const targetDir = '/tmp/cgr-template/';
+const realTargetDir = `${targetDir}guard-report-${branch}`;
+
+const options = { stdio: 'ignore' };
+
+// Printing info
+console.log(`Using ${inputPath} as the input`);
+
+// Prepare env
+execSync(`rm -rf ${targetFile} ${targetDir}`, options);
+
+// Download the zip
+console.log(`Downloading the template for branch ${branch}…`);
+execSync(`curl -L ${zipFile} --output ${targetFile}`, options);
+
+// Extract the zip and delete
+console.log('Extract the zip…');
+execSync(
+  `mkdir -p ${targetDir} && unzip ${targetFile} -d ${targetDir}`,
+  options
+);
+execSync(`rm -rf ${targetFile}`, options);
+
+// Copy the input
+execSync(`cp -a ${inputPath} ${realTargetDir}`);
+
+// Creating the report
+console.log('Creating the report…');
+const outputFile = `${process.cwd()}/Guard\\ Report.html`;
+execSync(
+  `cd ${realTargetDir} && npm run create-report && mv ${realTargetDir}/build/index.html ${outputFile}`,
+  options
+);
+
+// Cleanup
+console.log('Clean up…');
+execSync(`rm -rf ${targetDir}`);
+
+console.log(`Process done! Output file is in ${outputFile}`);
---
+++ scripts/parse.ts
@@ -0,0 +1,215 @@
+/**
+ * @param line of the shrinking usage
+ * @returns true if line is a valid class to be processed by the parser
+ */
+function isValidClass(line: string): boolean {
+  return (
+    line.startsWith('com.gojek') &&
+    !line.includes('$') &&
+    !line.includes('_') &&
+    !line.includes('.R') &&
+    !line.includes('BuildConfig') &&
+    !line.includes('Test') &&
+    !line.includes('Kt') && // KT file without class
+    !line.endsWith('Module') && // Dagger module
+    !line.endsWith('Component')
+  ); // Dagger component
+}
+
+/**
+ * Inidcate if a line is part of ViewBinding or DataBinding library
+ *
+ * @param line of the shrinking usage
+ * @returns true if the line indicate a binding class
+ */
+function isBindingClass(line: string): boolean {
+  return line.endsWith('Binding') && line.includes('databinding');
+}
+
+/**
+ * Parent class is usually indicate that the class is not removed
+ * but rather the fields or methods inside that class
+ *
+ * @param line of the shrinking usage
+ * @returns true if the line indicate a parent class
+ */
+function isParentClass(line: string): boolean {
+  return line.endsWith(':');
+}
+
+/**
+ * Identify internal symbol, it can be a field or methods
+ *
+ * @param line of the shrinking usage
+ * @returns true if the line indicate internal symbol
+ */
+function isInternalSymbol(line: string): boolean {
+  return line.startsWith('    ');
+}
+
+/**
+ * Check wheter the line is a symbol from Kotlin data class
+ *
+ * @param line of the shrinking usage
+ * @returns true if the line inidcate data class symbol
+ */
+function isDataClassSymbol(line: string): boolean {
+  return (
+    line.includes('copy(') ||
+    line.includes('copy$default(') ||
+    (line.includes('component') && line.endsWith('()'))
+  );
+}
+
+/**
+ * Check wheter the line is a class generate from Dagger library
+ *
+ * @param line of the shrinking usage
+ * @returns true if the line inidcate a dagger class
+ */
+function isPossiblyDaggerClass(line: string): boolean {
+  return (
+    line.endsWith('Scope') ||
+    (line.includes('dagger') && line.endsWith('Component')) ||
+    line.endsWith('Qualifier')
+  );
+}
+
+interface ProguardResultVisitor {
+  visitClass(line: string, isClassUnused: boolean): void;
+  visitInternalSymbol(line: string, parentClass: string): void;
+}
+
+interface ResultNode {
+  [parent: string]: {
+    tags: string[];
+    children: string[];
+  };
+}
+
+class ResultBuilder {
+  result: ResultNode = {};
+
+  addParent(parentClass: string) {
+    const child = this.getEmptyChild();
+    this.result[parentClass] = child;
+  }
+
+  addChild(parentClass: string, child: string) {
+    const children = this.result[parentClass].children;
+    children.push(child);
+  }
+
+  addTag(parentClass: string, tag: string) {
+    const children = this.result[parentClass].tags;
+
+    if (!children.includes(tag)) {
+      children.push(tag);
+    }
+  }
+
+  build(): string {
+    return JSON.stringify(this.result, null, 2);
+  }
+
+  private getEmptyChild() {
+    return {
+      tags: [],
+      children: [],
+    };
+  }
+}
+
+class ProguardResultVisitorImpl implements ProguardResultVisitor {
+  resultBuilder: ResultBuilder;
+
+  constructor(resultBuilder: ResultBuilder) {
+    this.resultBuilder = resultBuilder;
+  }
+
+  visitInternalSymbol(line: string, parentClass: string) {
+    if (isDataClassSymbol(line)) {
+      this.resultBuilder.addTag(parentClass, 'data');
+    } else {
+      this.resultBuilder.addChild(parentClass, line);
+    }
+  }
+
+  visitClass(line: string, isClassUnused: boolean) {
+    const resultBuilder = this.resultBuilder;
+
+    resultBuilder.addParent(line);
+
+    if (isClassUnused) {
+      resultBuilder.addTag(line, 'unused');
+    }
+
+    if (isPossiblyDaggerClass(line)) {
+      resultBuilder.addTag(line, 'dagger');
+    }
+
+    if (isBindingClass(line)) {
+      resultBuilder.addTag(line, 'binding');
+    }
+  }
+}
+
+/* Main */
+/* ------------------------------------------ */
+
+const fs = require('fs');
+const readline = require('readline');
+
+const inputPath = 'usage.txt';
+const outputPath = 'src/data.json';
+
+if (!fs.existsSync(inputPath)) {
+  throw new Error(`Input not exists at: ${inputPath}`);
+}
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream(inputPath),
+});
+
+const builder = new ResultBuilder();
+const visitor = new ProguardResultVisitorImpl(builder);
+
+let parentClass = '';
+
+lineReader.on('line', (line: string) => {
+  const valid = isValidClass(line);
+
+  if (valid) {
+    console.log(`Visit class ${line}`);
+
+    if (isParentClass(line)) {
+      const normalizedValue = line.replace(':', '');
+      visitor.visitClass(normalizedValue, false);
+      parentClass = normalizedValue;
+    } else {
+      visitor.visitClass(line, true);
+    }
+  } else if (isInternalSymbol(line) && parentClass) {
+    console.log(`visit internal $line : ${parentClass}`);
+    visitor.visitInternalSymbol(line.trim(), parentClass);
+  } else {
+    // Invalid class
+    parentClass = '';
+  }
+});
+
+lineReader.on('error', (err: Error) => {
+  throw err;
+});
+
+lineReader.on('close', () => {
+  const content = builder.build();
+  fs.writeFile(outputPath, content, (err: Error) => {
+    if (err) {
+      console.log('Error is happening!');
+      console.error(err);
+    } else {
+      console.log('Report parsing success!');
+    }
+  });
+});
